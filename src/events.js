34696b49b40e2dc2bef390083d53de99f1665c03
Matyas Buczko <whybuzz@gmail.com>
2017-08-13 19:05:52 +0100

    events updated for new event handling

--- src/events.js
+++ src/events.js
@@ -1,10 +1,8 @@
 import Flight from 'flight';
 
-export const FlowEvent = Flight.eventType(
-    function(step) {
-        this.step = step;
-    }
-);
+export const FlowEvent = Flight.defineEventType({
+    step: 'string'
+});
 
-export const ShowStepEvent = Flight.eventOfType(FlowEvent).alias('Flow-Manager:ShowStep');
-export const StepBackEvent = Flight.basicEvent().alias('Flow-Manager:Back');
+export const ShowStepEvent = Flight.defineEvent(FlowEvent, 'Flow-Manager:ShowStep');
+export const StepBackEvent = Flight.defineEvent(Flight.basicEvent(), 'Flow-Manager:Back');
